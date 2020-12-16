a888ecaae2d7f340cd215397e9d6bdcc7fd137ca
Anne Li <24998524+fibanneacci@users.noreply.github.com>
2020-12-15 21:32:07 -0600

    Add files via upload

---
+++ src/Spotify.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Spotify.css'
+
+const SpotifySearch = () => {
+    return (
+        <div className="Spotify-search">
+            <div className="Spotify-search-bar">
+                <label htmlFor="Spotify-search-text">Search Spotify: </label>
+                <input type="text" id="Spotify-search-text" name="Spotify-search-text" />
+            </div>
+            <div className="Spotify-search-result">
+                {/* */}
+            </div>
+            <div className="Spotify-search-result">
+                {/* */}
+            </div>
+            <div className="Spotify-search-result">
+                {/* */}
+            </div>
+            <div className="Spotify-search-result">
+                {/* */}
+            </div>
+            <div className="Spotify-search-result">
+                {/* */}
+            </div>
+        </div>
+    );
+}
+
+export default SpotifySearch;
