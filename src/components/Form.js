1157e6921461c484ca6351bb3b5ffc71e7f55911
teafang <37667251+teafang@users.noreply.github.com>
2020-12-19 22:57:28 -0700

    Update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,6 +3,35 @@ import './Form.css'
 import { Formik, Field } from 'formik';
 
 class Forminput extends React.Component {
+    validateForm() {
+        console.log('hi')
+        let error;
+        var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+          '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+          '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+          '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+          '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+          '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+        if (!!pattern.test(document.getElementById("link").value) == false) {
+            error = "Please enter a valid link"
+        } else {
+            error = "None"
+        }
+        console.log(error)  
+
+        if (error != "None") {
+            document.getElementById("errorMessage").value = error
+            document.getElementById("errorMessage").style.visibility = "visible"
+        }
+    }
+
+    textLength() {
+        if (document.getElementById("link").value.length >= 2) {
+            return 'over'
+        } 
+        console.log(document.getElementById("link").value)
+    }
+    
     render() {
         return (
             <div className='Form'>
@@ -27,17 +56,16 @@ class Forminput extends React.Component {
                         <form>
                             <div id="row">
                                 <p>link</p>
-                                <Field name="link" type="text" />
+                                <Field name="link" id="link" type="text"/>
                             </div>
                             <div>
                                 <p>upload</p>
-                                <Field name="upload" as="select" className="my-select">
-                                    <option value="blue">blue</option>
-                                </Field>
+                                <input name="upload" type="file"></input>
                             </div>
                             <div>
                                 <p>text</p>
-                                <Field name="text" as="textarea" className="formtextarea" />
+                                <Field name="text" as="textarea" className="formtextarea" onChange={this.onChange}/>
+
                             </div>
                             <div>
                                 <p>tags</p>
@@ -53,6 +81,8 @@ class Forminput extends React.Component {
                                 document.getElementsByName("text")[0].value,
                                 document.getElementsByName("tags")[0].value
                             ]) }>done!</button>
+//                             <onClick={this.validateForm}
+//                             <p id="erroronClick={this.validateFormMessage" style={{ visibility: `hidden` }}></p>
                         </form>
                     </Formik>
                 </div>
@@ -62,18 +92,3 @@ class Forminput extends React.Component {
 }
 
 export default Forminput;
-
-
-
-// validation code for future reference
-// validate={values => {
-//     let errors = {};
-//     if (!values.email) {
-//     errors.email = "Required";
-//     } else if (
-//     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-//     ) {
-//     errors.email = "Invalid email address";
-//     }
-//     return errors;
-// }}
