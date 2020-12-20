f971d59b5657db818ce649bdae581fa311f23394
Anne Li <24998524+fibanneacci@users.noreply.github.com>
2020-12-19 20:40:03 -0600

    Add files via upload

--- src/components/Spotify.js
+++ src/components/Spotify.js
@@ -9,22 +9,14 @@ const Spotify = (props) => {
             let propsLeft = props.url.substring(0, splitIndex + 5)
             let propsRight = props.url.substring(splitIndex + 5)
             return (
-<<<<<<< HEAD
                 <iframe src={"" + propsLeft + "embed-podcast/" + propsRight} style={{ left: props.x, top: props.y }} width="100%" height="232" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-=======
-                <iframe src={"" + propsLeft + "embed-podcast/" + propsRight} left={props.x} top={props.y} width="100%" height="232" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
->>>>>>> 126614a8803d471a397a4d86873c066a0f5c6a59
             );
         } else { // music
             let splitIndex = props.url.indexOf(".com")
             let propsLeft = props.url.substring(0, splitIndex + 5)
             let propsRight = props.url.substring(splitIndex + 5)
             return (
-<<<<<<< HEAD
                 <iframe src={"" + propsLeft + "embed/" + propsRight} style={{ left: props.x, top: props.y }} width="300px" height="380px" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-=======
-                <iframe src={"" + propsLeft + "embed/" + propsRight} left={props.x} top={props.y} width="300px" height="380px" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
->>>>>>> 126614a8803d471a397a4d86873c066a0f5c6a59
             );
         }
     }
--- src/components/Video.js
+++ src/components/Video.js
@@ -9,11 +9,7 @@ const Video = (props) => {
         let splitIndex2 = props.url.indexOf("watch?v=")
         let propsRight = props.url.substring(splitIndex2 + 8)
         return (
-<<<<<<< HEAD
             <iframe width="560" height="315" src={propsLeft + "-nocookie.com/embed/" + propsRight} style={{ left: props.x, top: props.y }} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-=======
-            <iframe width="560" height="315" src={propsLeft + "-nocookie.com/embed/" + propsRight} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
->>>>>>> 126614a8803d471a397a4d86873c066a0f5c6a59
         );
     }
 }
